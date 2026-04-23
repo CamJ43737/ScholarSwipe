146f4247101c4b2bbe1041ab102b4841385b87d5
Cameron <cameron.kalon@gmail.com>
2026-04-23 16:37:48 -0500

    Initial Commit

---
+++ ScholarsSwipe/ScholarsSwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScholarsSwipeApp.swift
+//  ScholarsSwipe
+//
+//  Created by Cameron Jones on 4/23/26.
+//
+
+import SwiftUI
+
+@main
+struct ScholarsSwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
